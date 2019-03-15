8fd20c6903fa29235f3ea31f6a444c7e16988831
System Administrator <root@Bogdans-MacBook-Pro.local>
2019-03-15 16:01:24 +0200

    Commit

--- application.py
+++ application.py
@@ -7,7 +7,6 @@ import subprocess
 if __name__ == '__main__':
     ip = '35.183.247.189'
     port = '4503'
-    key = 'xvfT9T4FRCWV=W@v?=+Q}GHL'
-
+    key = sys.argv[1]
     command = "/usr/src/app/push_publisher.sh " + key
     os.system(command)
