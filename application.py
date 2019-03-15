e6c304b0074ddab1c27896912919a2220dfdb9ab
System Administrator <root@Bogdans-MacBook-Pro.local>
2019-03-15 15:47:21 +0200

    Commit

--- application.py
+++ application.py
@@ -8,5 +8,6 @@ if __name__ == '__main__':
     ip = '35.183.247.189'
     port = '4503'
     key = 'xvfT9T4FRCWV=W@v?=+Q}GHL'
-    command = "/usr/src/app/push_publisher.sh " + 'http://' + ip + ':' + port + '/crx/packmgr/service.jsp' + ' ' + key
+
+    command = "/usr/src/app/push_publisher.sh " + key
     os.system(command)
