21f0c1ec9a2881dcf511355c746b9df836ee2af0
Bogdan <costin.bogdan.maria27@gmail.com>
2019-03-26 13:57:03 +0200

    lala

--- aem-apps-file/push_publisher.sh
+++
@@ -1,6 +0,0 @@
-  #!/bin/bash
-
-cd /usr/src/app/Archive
-zip -r /usr/src/app/Archive.zip jcr_root META-INF
-cd /usr/src/app
-curl -u admin:$1 -F file=@Archive.zip -F name="lala" -F force=true -F install=true http://35.183.247.189:4503/crx/packmgr/service.jsp
