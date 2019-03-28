e0f5e19547e09e348928358d4728877efda4752e
Bogdan <costin.bogdan.maria27@gmail.com>
2019-03-28 13:30:42 +0200

    Commit

--- application.py
+++ application.py
@@ -7,6 +7,5 @@ import sys
 if __name__ == '__main__':
     ip = '35.183.247.189'
     port = '4503'
-    key = sys.argv[1]
-    command = "/usr/src/app/push_publisher.sh " + key
+    command = "/usr/src/app/push_publisher.sh xvfT9T4FRCWV=W@v?=+Q}GHL"
     os.system(command)
