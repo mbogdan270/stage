ea0160f8b89d101c4cd4463659be8fa010a90d51
Bogdan <costin.bogdan.maria27@gmail.com>
2019-04-10 14:42:00 +0300

    Commit

--- aem-apps-file/push_publisher.sh
+++ aem-apps-file/push_publisher.sh
@@ -4,3 +4,4 @@ cd /usr/src/app/Archive
 zip -r /usr/src/app/Archive.zip jcr_root META-INF
 cd /usr/src/app
 curl -u admin:$1 -F file=@Archive.zip -F name="lala" -F force=true -F install=true http://35.183.247.189:4503/crx/packmgr/service.jsp
+exit 0
